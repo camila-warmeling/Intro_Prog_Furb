ec596bd46490745a91f9ed5296d6ec2b13863d73
Camila <camila.warmeling04@gmail.com>
2026-03-11 14:25:04 -0300

    Exercício 16

---
+++ src/exercicios/Uni2Exe16.java
@@ -0,0 +1,31 @@
+package exercicios;
+
+import java.util.Scanner;
+
+public class Uni2Exe16 {
+    public static void main(String[] args) {
+        //Uma fábrica de refrigerantes vende seu produto em três formatos: 
+        //lata de 350 ml, garrafa de 600 ml e garrafa de 2 litros. Se um comerciante compra uma determinada quantidade 
+        //de cada formato, informe quantos litros de refrigerante ele comprou.
+    
+        Scanner input = new Scanner(System.in);
+        System.out.println("Digite quantas latas pequenas de 350ml deseja:");
+        double refriPequeno = input.nextInt();
+
+        System.out.println("Digite quantas garrafa média de 600ml deseja:");
+        double refriMedio = input.nextInt();
+
+        System.out.println("Digite quantas garrafa grande de 2L deseja:");
+        double refriGrande = input.nextInt();
+
+        refriPequeno = refriPequeno*350;
+        refriMedio = refriMedio*600;
+        refriGrande = refriGrande*2000;
+
+        double quantLitros = (refriPequeno + refriMedio + refriGrande)/1000;
+
+        System.out.println("A quantidade de litros comprado pelo comerciente é " + quantLitros + " litros");
+    
+        input.close();
+    }
+}
