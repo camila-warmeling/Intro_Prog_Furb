e06224ebf489bccf9361df279b5cc4d2e08913c9
Camila <camila.warmeling04@gmail.com>
2026-03-28 20:50:29 -0300

    Exercício 10

---
+++ src/unidade4/exerciciosUni4/Uni4Exe10.java
@@ -0,0 +1,50 @@
+package unidade4.exerciciosUni4;
+
+import java.util.Scanner;
+
+//Um casal possui três filhos: Marquinhos, Zezinho e Luluzinha. Faça um algoritmo para ler as 
+//idades dos filhos e exibir quem é o caçula da família; suponha que não haja empates.
+
+public class Uni4Exe10 {
+
+    private int idadeFilhoMar, idadeFilhoZe, idadeFilhoLulu; 
+    private String filhoMaisNovo;
+
+    public Uni4Exe10(int idadeFilhoMar, int idadeFilhoZe, int idadeFilhoLulu){
+        this.idadeFilhoMar = idadeFilhoMar;
+        this.idadeFilhoZe = idadeFilhoZe;
+        this.idadeFilhoLulu = idadeFilhoLulu;
+    } 
+
+    public String verificarCaculaDaFamilia(){
+        if (this.idadeFilhoMar < this.idadeFilhoLulu || this.idadeFilhoZe < this.idadeFilhoLulu){
+            if(this.idadeFilhoMar < this.idadeFilhoZe){
+                filhoMaisNovo = "O filho mais novo é o Marquinhos";
+            }
+            else{
+                filhoMaisNovo = "O filho mais novo é o Zezinho";
+            }
+        }else{
+            filhoMaisNovo = "O filho mais novo é a Luluzinha";
+        }
+
+        return filhoMaisNovo;
+    }
+
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.err.println("Digite a idade de Marquinhos");
+        int idadeMarquinhos = input.nextInt();
+
+        System.err.println("Digite a idade de Zezinho");
+        int idadeZezinho = input.nextInt();
+
+        System.err.println("Digite a idade de Luluzinha");
+        int idadeLuluzinha = input.nextInt();
+
+        Uni4Exe10 classeExec = new Uni4Exe10(idadeMarquinhos, idadeZezinho, idadeLuluzinha);
+        System.err.println(classeExec.verificarCaculaDaFamilia());
+
+        input.close();
+    }
+}
