1cf98db90eb1b4785baeefb0f30b254e59d53c16
Camila <camila.warmeling04@gmail.com>
2026-04-02 19:15:20 -0300

    Exercício 15

---
+++ src/unidade4/exerciciosUni4/Uni4Exe15.java
@@ -0,0 +1,42 @@
+package unidade4.exerciciosUni4;
+
+import java.util.Scanner;
+
+//exibir o valor de reajuste que um funcionário receberá no seu salário. A empresa irá conceder 
+//5% de reajuste para o funcionário que for admitido há até de 12 meses. Para funcionário admitido 
+//entre 13 e 48 meses, irá conceder 7% de reajuste. O seu algoritmo deve solicitar ao usuário 
+//que digite a quantidade de meses que o funcionário foi admitido.
+
+public class Uni4Exe15 {
+
+    private int quantMeses;
+    private String reajusteFuncionario;
+
+    public Uni4Exe15(int quantMeses){
+        this.quantMeses = quantMeses;
+    }
+
+    public String verigicarPorcentagemReajuste(){
+        if (this.quantMeses > 0 && this.quantMeses <= 12){
+            reajusteFuncionario = ("O funcionário receberá 5% de ajuste no salário");
+        }
+        else if (this.quantMeses > 12 && this.quantMeses <=48){
+            reajusteFuncionario = ("O funcionário receberá 7% de ajuste no salário");
+        }
+        else{
+            reajusteFuncionario = ("Não haverá reajuste no salário do funcionário");
+        }
+
+        return reajusteFuncionario;
+    }
+
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.err.println("Digite a quantidade de meses que o funcionário foi admitido:");
+        int meses = input.nextInt();
+
+        Uni4Exe15 classeExec = new Uni4Exe15(meses);
+        System.err.println(classeExec.verigicarPorcentagemReajuste());
+    }
+
+}
