61130df685d20a7dbfc69f6a37fe1828f4f2c730
Camila <camila.warmeling04@gmail.com>
2026-04-08 19:59:26 -0300

    .

---
+++ src/unidade4/exerciciosUni4/Uni4Exe21.java
@@ -0,0 +1,21 @@
+package unidade4.exerciciosUni4;
+
+//O índice de massa corporal (IMC) é uma medida internacional usada para calcular se uma pessoa 
+//está no peso ideal. O IMC é determinado pela divisão da massa do indivíduo pelo quadrado de sua 
+//altura, onde a massa está em quilogramas e a altura está em metros, de acordo com a fórmula:
+
+//IMC = massa/altura²
+
+//Faça um algoritmo para classificar o IMC e dizer o grau de obesidade do indivíduo, de acordo:
+
+//IMC	        Classificação
+//< 18.5	      Magreza
+//18.5 - 24.9	  Saudável
+//25.0 - 29.9  	  Sobrepeso
+//30.0 - 34.9	  Obesidade Grau I
+//35.0 - 39.9	  Obesidade Grau II (severa)
+//>= 40.0	      Obesidade Grau III (mórbida)
+
+public class Uni4Exe21 {
+
+}
