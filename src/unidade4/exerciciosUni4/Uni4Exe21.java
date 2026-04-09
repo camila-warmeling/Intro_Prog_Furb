7855d06524e94e53288fd0fdfb1d8e33c0f26015
Camila <camila.warmeling04@gmail.com>
2026-04-08 21:02:31 -0300

    Exercício 21

--- src/unidade4/exerciciosUni4/Uni4Exe21.java
+++ src/unidade4/exerciciosUni4/Uni4Exe21.java
@@ -1,22 +1,62 @@
 package unidade4.exerciciosUni4;
 
-//a massa está em quilogramas e a altura está em metros, de acordo com a fórmula:
-//IMC = massa/altura²
-//Faça um algoritmo para classificar o IMC e dizer o grau de obesidade do indivíduo, de acordo:
-//IMC	        Classificação
-//< 18.5	      Magreza
-//18.5 - 24.9	  Saudável
-//25.0 - 29.9  	  Sobrepeso
-//30.0 - 34.9	  Obesidade Grau I
-//35.0 - 39.9	  Obesidade Grau II (severa)
-//>= 40.0	      Obesidade Grau III (mórbida)
+import java.util.Scanner;
 
 public class Uni4Exe21 {
 
-    Scanner input = new Scanner(System.in);
-    private float massa, altura;
-    private String classificacao;
+    private double massa, altura, IMC;
+    private String classificacaoIMC;
 
+    public Uni4Exe21 (double massa, double altura){
+        this.massa = massa;
+        this.altura = altura;
+    } 
 
+    public void calcularIMC(){
+        IMC = this.massa / Math.pow(this.altura, 2);
+    }
 
+    public String classificarIMC(){
+        if(IMC < 18.5){
+            classificacaoIMC = "Magreza";
+        }
+        else if(IMC < 25){
+            classificacaoIMC = "Saudável";
+        }
+        else if(IMC < 30){
+            classificacaoIMC = "Sobrepeso";
+        }
+        else if(IMC < 35){
+            classificacaoIMC = "Obesidade Grau I";
+        }
+        else if(IMC < 40){
+            classificacaoIMC = "Obesidade Grau II (severa)";
+        }
+        else{
+            classificacaoIMC = "Obesidade Grau III (mórbida)";
+        }
+
+        return classificacaoIMC;
+    }
+
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.println("Digite a massa em quilogramas:");
+        double massaIndividuo = input.nextFloat();
+
+        System.out.println("Digite a altura em metros:");
+        double alturaIndividuo = input.nextFloat();
+
+        if(massaIndividuo > 0 && alturaIndividuo > 0){
+            Uni4Exe21 classeExec = new Uni4Exe21(massaIndividuo, alturaIndividuo);
+            classeExec.calcularIMC();
+            System.out.println("A classificação do indivíduo é " + classeExec.classificarIMC());
+        }
+        
+        else{
+            System.out.println("Dados Inválidos!");
+        }
+
+        input.close();
+    }
 }
