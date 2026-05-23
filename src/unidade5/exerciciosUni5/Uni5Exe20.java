e36f139a5e86352de58c1759b511549e871e15ca
Camila <camila.warmeling04@gmail.com>
2026-05-23 11:07:15 -0300

    exercício 20

--- src/unidade5/exerciciosUni5/Uni5Exe20.java
+++ src/unidade5/exerciciosUni5/Uni5Exe20.java
@@ -1,30 +1,31 @@
 package unidade5.exerciciosUni5;
 
+import java.util.Locale;
 import java.util.Scanner;
 
 public class Uni5Exe20 {
 
     public static void main(String[] args) {
         
-        Scanner input = new Scanner(System.in);
+        Scanner input = new Scanner(System.in).useLocale(Locale.US);;
         double massaInicial, massaFinal;
-        int segundos = 00, minutos = 00, horas = 00;
+        int tempo = 0, segundos = 00, minutos = 00, horas = 00;
 
         System.out.println("Digite a massa inicial:");
         massaInicial = input.nextDouble();
 
         massaFinal = massaInicial;
         
-        while (massaFinal > 0.0005){
+        while (massaFinal >= 0.0005){
             massaFinal /= 2;
-            segundos += 50;    
+            tempo += 50;    
         }
 
-        horas = segundos/3600;
-        minutos = (segundos%3600)/60;
-        segundos = segundos%60;
+        horas = tempo/3600;
+        minutos = (tempo%3600)/60;
+        segundos = tempo%60;
 
-        System.out.printf("Massa Inicial: %.3fkg\nMassa Final: %.3fkg\nTempo: %d:%d:%d", massaInicial, massaFinal, horas, minutos, segundos);
+        System.out.printf("Massa Inicial: %fkg\nMassa Final: %fkg\nTempo: %d:%d:%d", massaInicial, massaFinal, horas, minutos, segundos);
 
         input.close();
     }
