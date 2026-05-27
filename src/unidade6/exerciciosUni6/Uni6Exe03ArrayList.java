21c1484a730120123c330fca464330d4b8306985
Camila <camila.warmeling04@gmail.com>
2026-05-27 19:21:15 -0300

    Exercício 3 em vetor e array list

---
+++ src/unidade6/exerciciosUni6/Uni6Exe03ArrayList.java
@@ -0,0 +1,48 @@
+package unidade6.exerciciosUni6;
+
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class Uni6Exe03ArrayList {
+
+    public int tamanho = 12;
+    public ArrayList<Double> valores = new ArrayList<>();
+    public Scanner input = new Scanner(System.in);
+
+    public Uni6Exe03ArrayList(){
+
+    }
+
+    public void lerValores(){
+        for(int i=0; i<tamanho; i++){
+            System.out.println("Digite o valor:");
+            double valor = input.nextDouble();
+            valores.add(valor);
+        }
+    }
+
+    public void alterarValores(){
+        for(int i=0; i<tamanho; i++){
+            if(i % 2 == 0){
+                //par aumenta o valor em 2%
+                valores.set(i, valores.get(i) * 1.02);
+            }else{
+                //impar aumenta o valor em 5%
+                valores.set(i, valores.get(i) * 1.05);
+            }
+        }
+    }
+
+    public void imprimir(){
+        for(int i=0; i<tamanho; i++){
+            System.out.println("Posição "+i+" valor: " + String.format("%.2f",valores.get(i)));
+        }
+    }
+
+    public static void main(String[] args) {
+        Uni6Exe03Vetor exec03 = new Uni6Exe03Vetor();
+        exec03.lerValores();
+        exec03.alterarValores();
+        exec03.imprimir();
+    }
+}
---
+++ src/unidade6/exerciciosUni6/Uni6Exe03Vetor.java
@@ -0,0 +1,46 @@
+package unidade6.exerciciosUni6;
+
+import java.util.Scanner;
+
+public class Uni6Exe03Vetor {
+
+    public int tamanho = 12;
+    public double[] valores = new double[tamanho];
+    public Scanner input = new Scanner(System.in);
+
+    public Uni6Exe03Vetor(){
+
+    }
+
+    public void lerValores(){
+        for(int i=0; i<tamanho; i++){
+            System.out.println("Digite o valor:");
+            valores[i] = input.nextDouble();
+        }
+    }
+
+    public void alterarValores(){
+        for(int i=0; i<tamanho; i++){
+            if(i % 2 == 0){
+                //par aumenta o valor em 2%
+                valores[i] *= 1.02;
+            }else{
+                //impar aumenta o valor em 5%
+                valores[i] *= 1.05;
+            }
+        }
+    }
+
+    public void imprimir(){
+        for(int i=0; i<tamanho; i++){
+            System.out.println("Posição "+i+" valor: " + String.format("%.2f",valores[i]));
+        }
+    }
+
+    public static void main(String[] args) {
+        Uni6Exe03Vetor exec03 = new Uni6Exe03Vetor();
+        exec03.lerValores();
+        exec03.alterarValores();
+        exec03.imprimir();
+    }
+}
