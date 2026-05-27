7581084dbdbee2e9b0f2a3f3ba42a94b94ebed30
Camila <camila.warmeling04@gmail.com>
2026-05-27 19:45:55 -0300

    Exercício 30

--- src/unidade5/exerciciosUni5/Uni5Exe30.java
+++ src/unidade5/exerciciosUni5/Uni5Exe30.java
@@ -3,20 +3,11 @@ package unidade5.exerciciosUni5;
 import java.util.Scanner;
 
 public class Uni5Exe30 {
-//Implemente o problema da mochila. Tendo-se uma sequência decrescente de números inteiros positivos 
-//que inicia em N, com decremento inteiro positivo K, deseja-se empacotá-los em uma mochila com tamanho 
-//M, de forma que se coloque dentro dela preferencialmente os maiores valores, até que ela esteja cheia. 
-//N e K são inteiros e devem ser definidos pelo usuário. Faça um algoritmo que imprima:
-//os elementos a serem colocados na mochila;
-//os elementos que entraram na mochila;
-//os que ficaram fora da mochila;
-//qual é a soma dos elementos que entraram na mochila;
-//qual a soma dos elementos que não entraram na mochila.
 
     public static void main(String[] args) {
         
         Scanner input = new Scanner(System.in);
-        int numero, decremento, tamanhoMochila, somaElementosDentro, somaElementosFora;
+        int numero, decremento, tamanhoMochila, somaElementosDentro = 0, somaElementosFora = 0;
         String elementosSeremColocados = "", elementosDentro = "", elementosFora = "";
 
         System.out.println("Digite o número que vai iniciar:");
@@ -30,11 +21,20 @@ public class Uni5Exe30 {
             if(tamanhoMochila > 0){
                 tamanhoMochila --;
                 elementosDentro += numero + " ";
+                somaElementosDentro += numero;
             }else{
                 elementosFora += numero + " ";
+                somaElementosFora += numero;
             }
             elementosSeremColocados += numero + " ";
+            numero -= decremento;
         }
+
+        System.out.println("Elementos a serem colocados na mochila: " + elementosSeremColocados);
+        System.out.println("Elementos que entraram na mochila: " + elementosDentro);
+        System.out.println("Elementos que não entraram na mochila: " + elementosFora);
+        System.out.println("Soma dos elementos que entraram na mochila: " + somaElementosDentro);
+        System.out.println("Soma dos elementos que não entraram na mochila: " + somaElementosFora);
 
         input.close();
     }
