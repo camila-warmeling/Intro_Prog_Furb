21d6fcab603349365ae5516ea81a46d5f04ec0ef
Camila <camila.warmeling04@gmail.com>
2026-05-27 18:43:28 -0300

    Exercício 1 da unidade 6 com vetores e array list

---
+++ src/unidade6/exerciciosUni6/Uni6Exe01ArrayList.java
@@ -0,0 +1,34 @@
+package unidade6.exerciciosUni6;
+
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class Uni6Exe01ArrayList {
+
+    Scanner input = new Scanner(System.in);
+    //começa vazio
+    public ArrayList<Integer> numeros = new ArrayList<>();
+
+    public Uni6Exe01ArrayList(){
+        lerDados();
+        mostrarNumerosInverso();
+    }
+
+    public void lerDados(){
+        for(int i=0; i<10; i++){
+            System.out.println("Digite o " + (i+1) + "º número");
+            int valor = input.nextInt();
+            numeros.add(valor);
+        }
+    }
+
+    public void mostrarNumerosInverso(){
+        for(int i=9; i>=0; i--){
+            System.out.println(numeros.get(i));
+        }
+    }
+    
+    public static void main(String[] args) {
+        new Uni6Exe01ArrayList();
+    }
+}
--- src/unidade6/exerciciosUni6/Uni6Exe01Vetor.java
+++ src/unidade6/exerciciosUni6/Uni6Exe01Vetor.java
@@ -2,14 +2,12 @@ package unidade6.exerciciosUni6;
 
 import java.util.Scanner;
 
-public class Uni6Exe01 {
-//Descreva um algoritmo que leia 10 números inteiros e os coloque em um vetor de 10 posições do tipo 
-//inteiro. Escreva na ordem inversa em que foram lidos. Faça um método para ler e outro para escrever.
+public class Uni6Exe01Vetor {
 
     Scanner input = new Scanner(System.in);
     public int[] numeros = new int [10];
 
-    public Uni6Exe01(){
+    public Uni6Exe01Vetor(){
         lerDados();
         mostrarNumerosInverso();
     }
@@ -26,8 +24,8 @@ public class Uni6Exe01 {
             System.out.println(numeros[i]);
         }
     }
-
+    
     public static void main(String[] args) {
-        new Uni6Exe01();
+        new Uni6Exe01Vetor();
     }
 }
