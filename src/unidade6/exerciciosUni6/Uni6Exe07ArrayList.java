7f9b4e3ca4bb9b88c184c482d1f8a065a3fa0bde
Camila <camila.warmeling04@gmail.com>
2026-06-07 11:16:05 -0300

    .

--- src/unidade6/exerciciosUni6/Uni6Exe07ArrayList.java
+++ src/unidade6/exerciciosUni6/Uni6Exe07ArrayList.java
@@ -4,15 +4,6 @@ import java.util.ArrayList;
 import java.util.Scanner;
 
 public class Uni6Exe07ArrayList {
-// Faça um programa que leia um valor N inteiro limitado a 20 posições. Com base neste valor, crie um 
-//vetor do tipo inteiro. Em seguida, solicite ao usuário para digitar um número inteiro várias vezes, 
-//até preencher o vetor. Esse número deverá ser armazenado no vetor caso ainda não exista, e se o valor 
-//já existir deve ser pedido um novo valor. Isto é, se o usuário informar o número 2 e já existir o número 
-//2 em alguma posição do vetor, o número não deve ser adicionado. Após ter adicionado todos os números 
-//dentro do vetor, o mesmo deve ser ordenado. Para isso reordene os elementos internos do vetor de modo 
-//que este fique em ordem crescente, conforme no exemplo:
-//Vetor origem: [0][4][2][6][3]
-//Vetor ordenado: [0][2][3][4][6]
 
     public Scanner input = new Scanner(System.in);
     public ArrayList<Integer> listaNumeros = new ArrayList<>();
